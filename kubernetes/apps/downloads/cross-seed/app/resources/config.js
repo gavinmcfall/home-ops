5a6d6d4c267aa3e0de39331b3d81c9a4920f2d39
gavinmcfall <gavin@nerdz.co.nz>
2024-01-18 19:05:10 +1300

    feat(sabnzbd) Added Tailscale to url whitelist

--- kubernetes/apps/downloads/cross-seed/app/resources/config.js
+++ kubernetes/apps/downloads/cross-seed/app/resources/config.js
@@ -1,28 +1,28 @@
 // Note: Cross-Seed vars should be escaped with $${VAR_NAME} to avoid interpolation by Flux
 module.exports = {
   delay: 20,
-  qbittorrentUrl: "http://qbittorrent.ddownloads.svc.cluster.local",
+  qbittorrentUrl: "http://qbittorrent.downloads.svc.cluster.local",
   torznab: [
-    `http://prowlarr.ddownloads.svc.cluster.local/49/api?apikey=$${process.env.PROWLARR_API_KEY}`, // atr
-    `http://prowlarr.ddownloads.svc.cluster.local/47/api?apikey=$${process.env.PROWLARR_API_KEY}`, // ar
-    `http://prowlarr.ddownloads.svc.cluster.local/48/api?apikey=$${process.env.PROWLARR_API_KEY}`, // ant
-    `http://prowlarr.ddownloads.svc.cluster.local/2/api?apikey=$${process.env.PROWLARR_API_KEY}`,  // avz
-    `http://prowlarr.ddownloads.svc.cluster.local/45/api?apikey=$${process.env.PROWLARR_API_KEY}`, // bhd
-    `http://prowlarr.ddownloads.svc.cluster.local/21/api?apikey=$${process.env.PROWLARR_API_KEY}`, // blu
-    `http://prowlarr.ddownloads.svc.cluster.local/11/api?apikey=$${process.env.PROWLARR_API_KEY}`, // btn
-    `http://prowlarr.ddownloads.svc.cluster.local/1/api?apikey=$${process.env.PROWLARR_API_KEY}`,  // fl
-    `http://prowlarr.ddownloads.svc.cluster.local/39/api?apikey=$${process.env.PROWLARR_API_KEY}`, // hds
-    `http://prowlarr.ddownloads.svc.cluster.local/44/api?apikey=$${process.env.PROWLARR_API_KEY}`, // hdt
-    `http://prowlarr.ddownloads.svc.cluster.local/38/api?apikey=$${process.env.PROWLARR_API_KEY}`, // ipt
-    `http://prowlarr.ddownloads.svc.cluster.local/26/api?apikey=$${process.env.PROWLARR_API_KEY}`, // mtv
-    `http://prowlarr.ddownloads.svc.cluster.local/84/api?apikey=$${process.env.PROWLARR_API_KEY}`, // nbl
-    `http://prowlarr.ddownloads.svc.cluster.local/8/api?apikey=$${process.env.PROWLARR_API_KEY}`,  // ptp
-    `http://prowlarr.ddownloads.svc.cluster.local/42/api?apikey=$${process.env.PROWLARR_API_KEY}`, // phd
-    `http://prowlarr.ddownloads.svc.cluster.local/6/api?apikey=$${process.env.PROWLARR_API_KEY}`,  // st
-    `http://prowlarr.ddownloads.svc.cluster.local/46/api?apikey=$${process.env.PROWLARR_API_KEY}`, // td
-    `http://prowlarr.ddownloads.svc.cluster.local/41/api?apikey=$${process.env.PROWLARR_API_KEY}`, // ts
-    `http://prowlarr.ddownloads.svc.cluster.local/20/api?apikey=$${process.env.PROWLARR_API_KEY}`, // tl
-    `http://prowlarr.ddownloads.svc.cluster.local/40/api?apikey=$${process.env.PROWLARR_API_KEY}`, // uhdb
+    `http://prowlarr.downloads.svc.cluster.local/49/api?apikey=$${process.env.PROWLARR_API_KEY}`, // atr
+    `http://prowlarr.downloads.svc.cluster.local/47/api?apikey=$${process.env.PROWLARR_API_KEY}`, // ar
+    `http://prowlarr.downloads.svc.cluster.local/48/api?apikey=$${process.env.PROWLARR_API_KEY}`, // ant
+    `http://prowlarr.downloads.svc.cluster.local/2/api?apikey=$${process.env.PROWLARR_API_KEY}`,  // avz
+    `http://prowlarr.downloads.svc.cluster.local/45/api?apikey=$${process.env.PROWLARR_API_KEY}`, // bhd
+    `http://prowlarr.downloads.svc.cluster.local/21/api?apikey=$${process.env.PROWLARR_API_KEY}`, // blu
+    `http://prowlarr.downloads.svc.cluster.local/11/api?apikey=$${process.env.PROWLARR_API_KEY}`, // btn
+    `http://prowlarr.downloads.svc.cluster.local/1/api?apikey=$${process.env.PROWLARR_API_KEY}`,  // fl
+    `http://prowlarr.downloads.svc.cluster.local/39/api?apikey=$${process.env.PROWLARR_API_KEY}`, // hds
+    `http://prowlarr.downloads.svc.cluster.local/44/api?apikey=$${process.env.PROWLARR_API_KEY}`, // hdt
+    `http://prowlarr.downloads.svc.cluster.local/38/api?apikey=$${process.env.PROWLARR_API_KEY}`, // ipt
+    `http://prowlarr.downloads.svc.cluster.local/26/api?apikey=$${process.env.PROWLARR_API_KEY}`, // mtv
+    `http://prowlarr.downloads.svc.cluster.local/84/api?apikey=$${process.env.PROWLARR_API_KEY}`, // nbl
+    `http://prowlarr.downloads.svc.cluster.local/8/api?apikey=$${process.env.PROWLARR_API_KEY}`,  // ptp
+    `http://prowlarr.downloads.svc.cluster.local/42/api?apikey=$${process.env.PROWLARR_API_KEY}`, // phd
+    `http://prowlarr.downloads.svc.cluster.local/6/api?apikey=$${process.env.PROWLARR_API_KEY}`,  // st
+    `http://prowlarr.downloads.svc.cluster.local/46/api?apikey=$${process.env.PROWLARR_API_KEY}`, // td
+    `http://prowlarr.downloads.svc.cluster.local/41/api?apikey=$${process.env.PROWLARR_API_KEY}`, // ts
+    `http://prowlarr.downloads.svc.cluster.local/20/api?apikey=$${process.env.PROWLARR_API_KEY}`, // tl
+    `http://prowlarr.downloads.svc.cluster.local/40/api?apikey=$${process.env.PROWLARR_API_KEY}`, // uhdb
   ],
   port: process.env.CROSSSEED_PORT || 80,
   apiAuth: false,
