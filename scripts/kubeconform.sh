16b8decc6e85d62cdbeb1e6764e019ed8d52ce71
gavinmcfall <gavin@nerdz.co.nz>
2024-08-05 08:29:10 +1200

    fix(kubeconform): Ignore

--- scripts/kubeconform.sh
+++ scripts/kubeconform.sh
@@ -12,7 +12,7 @@ kubeconform_args=(
     "-strict"
     "-ignore-missing-schemas"
     "-skip"
-    "Secret,ReplicationDestination.volsync.backube,ReplicationSource.volsync.backube"
+    "Secret,ReplicationSource,ReplicationDestination"
     "-schema-location"
     "default"
     "-schema-location"
