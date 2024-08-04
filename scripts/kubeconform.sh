d5df78b970425415e12cfa253baeef3ee37ea76b
gavinmcfall <gavin@nerdz.co.nz>
2024-08-05 08:25:02 +1200

    fix(kubeconform): Volsync

--- scripts/kubeconform.sh
+++ scripts/kubeconform.sh
@@ -12,7 +12,7 @@ kubeconform_args=(
     "-strict"
     "-ignore-missing-schemas"
     "-skip"
-    "Secret,ReplicationDestination,ReplicationSource"  # Added ReplicationDestination and ReplicationSource
+    "Secret,ReplicationDestination.volsync.backube,ReplicationSource.volsync.backube"
     "-schema-location"
     "default"
     "-schema-location"
